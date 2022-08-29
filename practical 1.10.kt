dc5b001dcc1a242561ee2fa94f2f83ecea6caa69
ompatel3105 <112079016+ompatel3105@users.noreply.github.com>
2022-08-29 22:11:54 +0530

    Add files via upload

---
+++ practical 1.10.kt
@@ -0,0 +1,29 @@
+class Car(type:String, model:String, price:Int, owner:String, miles:Int){
+    var ty:String = type
+    var mo =model
+    var p:Int = price
+    var ow = owner
+    var m:Int = miles
+    fun getCarInformation() {
+        println("Selling Price of Car: " + (p - m).toString() + "rs")
+        println("Actual price: " + p.toString() + "rs")
+        println("Total Kms: " + m.toString())
+        println("car model:" + mo.toString())
+        println("car owner: " + ow.toString())
+        println("car miles: " + m.toString())
+        println()
+    }
+    fun getOriginalCarPrice() {
+        println("Actual price: " + p.toString() + "rs")
+        println()
+    }
+}
+fun main() {
+    var c1 = Car("SUV", "Polo", 125000, "Husain", 10000)
+    c1.getCarInformation()
+    var c2 = Car(" SUV", "Baleno", 100000, "naresh", 50000)
+    c2.getCarInformation()
+    println("========================================")
+    c1.getOriginalCarPrice()
+    c2.getOriginalCarPrice()
+}
---
+++ practical1.1.kt
@@ -0,0 +1,21 @@
+fun main()
+{
+    var a=12
+    var b=1.7
+    var c= 'R'
+    var d="SRR"
+    var e=true
+    var f=67.89
+    var g=97881791338
+    var h=-2
+    var i=967
+    println("This is Integer Value:" +a)
+    println("This is Float Value:"+b)
+    println("This is Character Value:"+c)
+    println("This is String Value:"+d)
+    println("This is Boolean Value:"+e)
+    println("This is Double Value:"+f)
+    println("This is Long Value:"+e)
+    println("This is Short Value:"+h)
+    println("This is Byte Value:"+i)
+}
